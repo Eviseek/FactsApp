ba7380c593b3428f49ef49e12da32c9e44e45180
Eva Chlpikova <chlpikova.e@gmail.com>
2025-04-27 17:18:23 +0200

    Initial Commit

---
+++ FactsApp/FactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactsAppApp.swift
+//  FactsApp
+//
+//  Created by Eva Chlpikova on 27.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
